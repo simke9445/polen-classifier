13f92112d528b1385a50daa3d0180382aeaeba89
simke9445 <simke9445@gmail.com>
2016-02-13 20:00:06 +0100

    fixed variable names

--- binarizer.py
+++ binarizer.py
@@ -4,9 +4,8 @@ from sklearn.preprocessing import label_binarize
 
 def binarize(y):
     # translating features to a binary vector
-    min = np.min(y)
-    max = np.max(y)
-    temp = label_binarize(y, classes=range(min, max + 1))
+    y_min = np.min(y)
+    y_max = np.max(y)
+    temp = label_binarize(y, classes=range(y_min, y_max + 1))
 
-    assert isinstance(temp, object)
     return temp
