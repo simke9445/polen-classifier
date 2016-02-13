89015cfd32edbdd53c077ac0daa2351a28c8b51e
simke9445 <simke9445@gmail.com>
2016-02-13 02:50:47 +0100

    add fscore evaluation and fscore plot

---
+++ fscore.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.preprocessing import binarize
+
+
+def fscore(clf, X_test, y_test):
+    # binarize output vector
+    y_test = binarize(y_test)
+    print('y_test binarized shape = ', np.shape(y_test))
+    n_classes = np.shape(y_test)[1]
+
+    y_score = clf.predict_proba(X_test)
+
+    # Compute Precision-Recall and plot curve
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    for i in range(n_classes):
+        precision[i], recall[i], _ = precision_recall_curve(y_test[:, i],
+                                                            y_score[:, i])
+        average_precision[i] = average_precision_score(y_test[:, i], y_score[:, i])
+
+    # Compute micro-average ROC curve and ROC area
+    precision["micro"], recall["micro"], _ = precision_recall_curve(y_test.ravel(),
+                                                                    y_score.ravel())
+    average_precision["micro"] = average_precision_score(y_test, y_score,
+                                                         average="micro")
+
+    # Plot Precision-Recall curve
+    plt.clf()
+    plt.plot(recall[0], precision[0], label='Precision-Recall curve')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Precision-Recall example: AUC={0:0.2f}'.format(average_precision[0]))
+    plt.legend(loc="lower left")
+    plt.show()
+
+    # Plot Precision-Recall curve for each class
+    plt.clf()
+    plt.plot(recall["micro"], precision["micro"],
+             label='micro-average Precision-recall curve (area = {0:0.2f})'
+                   ''.format(average_precision["micro"]))
+    for i in range(n_classes):
+        plt.plot(recall[i], precision[i],
+                 label='Precision-recall curve of class {0} (area = {1:0.2f})'
+                       ''.format(i, average_precision[i]))
+
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Extension of Precision-Recall curve to multi-class')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    return plt
